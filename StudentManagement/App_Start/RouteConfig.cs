5d53b1e2fec7ee92f056e184d52bf429dbc9c467
hieu03012000 <hieu03012000@gmail.com>
2020-10-31 15:53:48 +0700

    login

--- StudentManagement/App_Start/RouteConfig.cs
+++ StudentManagement/App_Start/RouteConfig.cs
@@ -13,11 +13,6 @@ namespace StudentManagement
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
         }
     }
 }
---
+++ StudentManagement/Areas/Auth/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentManagement.Areas.Auth.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
